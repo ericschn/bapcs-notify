8bbf66a0bb83794b134650a0c55c1c218931453f
ericschn <eschneider327@gmail.com>
2023-04-02 13:39:51 -0500

    beginning to parse reddit json

--- api/app.js
+++ api/app.js
@@ -1,11 +1,45 @@
 const express = require('express');
+const axios = require('axios');
+
 const app = express();
 const port = 3000;
+
+let prevRedditJson = null;
 
 app.get('/', (req, res) => {
   res.send('<h1>API running</h1>');
 });
 
-app.listen (port, () => {
+app.get('/fetch-new', (req, res) => {
+  let newRedditJson = null;
+  axios.get('https://www.reddit.com/r/buildapcsales/new/.json').then((axiosRes) => {
+    newRedditJson = axiosRes.data.data.children;
+    if (compareRedditJson(prevRedditJson, newRedditJson)) {
+      console.log("Json already exists in memory, no processing needed");
+    } else {
+      console.log("Json has new data, parsing json now");
+      // parse json
+      prevRedditJson = newRedditJson;
+    }
+    res.send(prevRedditJson);
+    parseRedditJson(prevRedditJson);
+  });
+  // res.send(redditJsonFetch);
+});
+
+app.listen(port, () => {
   console.log(`API running on port ${port}`);
-})
+});
+
+function compareRedditJson(a, b) {
+  if (!a || !b) return false;
+  if (a[0].data.title === b[0].data.title) return true;
+}
+
+function parseRedditJson(posts) {
+  let result = [];
+  for (let post of posts) {
+    result.push(post.data.title);
+  }
+  console.log(result);
+}
