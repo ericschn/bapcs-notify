81e3d9b088252a365f6f854a426810e799ab8084
ericschn <eschneider327@gmail.com>
2023-05-20 14:52:38 -0500

    api prefix for routes

--- api/app.mjs
+++ api/app.mjs
@@ -8,40 +8,44 @@ import { loginRouter } from './routes/login.mjs';
 const app = express();
 const port = 3000;
 const postsCollection = db.collection('posts');
+const router = express.Router();
 
 initializeApp();
 
+// set route prefix
+app.use('/api/v1', router);
+
 app.use(cors());
 
-// IP whitelist
-app.use((req, res, next) => {
-  let validIps = [
-    '::ffff:127.0.0.1',
-    process.env.IP_LOCAL,
-    process.env.IP_HOME,
-  ];
+// TODO: api keys, jwt
+// app.use((req, res, next) => {
+//   let validIps = [
+//     '::ffff:127.0.0.1',
+//     process.env.IP_LOCAL,
+//     process.env.IP_HOME,
+//   ];
 
-  if (validIps.includes(req.socket.remoteAddress)) {
-    next();
-  } else {
-    // TODO: real log
-    console.log('IP not on whitelist: ' + req.socket.remoteAddress);
-    const err = new Error('Not Allowed');
-    next(err);
-  }
-});
+//   if (validIps.includes(req.socket.remoteAddress)) {
+//     next();
+//   } else {
+//     // TODO: real log
+//     console.log('IP not on whitelist: ' + req.socket.remoteAddress);
+//     const err = new Error('Not Allowed');
+//     next(err);
+//   }
+// });
 
-app.use((err, req, res, next) => {
-  // console.error(err.stack);
-  res.status(403).send('Forbidden');
-});
+// app.use((err, req, res, next) => {
+//   // console.error(err.stack);
+//   res.status(403).send('Forbidden');
+// });
 
-app.use('/fetch-new', fetchNewRouter);
-app.use('/posts', postsRouter);
-app.use('/login', loginRouter);
+router.use('/fetch-new', fetchNewRouter);
+router.use('/posts', postsRouter);
+router.use('/login', loginRouter);
 
-app.get('/', (req, res) => {
-  res.send('API');
+router.get('/', (req, res) => {
+  res.send('API - /api/v1');
 });
 
 app.listen(port, () => {
