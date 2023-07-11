f73a810bf08ec31a679dfa25bb683430097f92b6
ericschn <eschneider327@gmail.com>
2023-07-10 20:09:17 -0500

    adding more login routes, clean up app.mjs

--- api/app.mjs
+++ api/app.mjs
@@ -1,54 +1,28 @@
 import express from 'express';
 import cors from 'cors';
-import db from './db/conn.mjs';
+import cookieParser from 'cookie-parser';
 import { workerRouter, initializeApp } from './routes/worker.mjs';
 import { postsRouter } from './routes/posts.mjs';
-import { loginRouter } from './routes/login.mjs';
+import { userRouter } from './routes/userRoutes.mjs';
 
 const app = express();
 const port = process.env.PORT;
-const postsCollection = db.collection('posts');
 const router = express.Router();
 
 initializeApp();
-
 app.use(express.json());
-
-// set route prefix
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(cors());
 app.use('/api/v1', router);
-
-router.use(cors());
-
-// TODO: api keys, jwt
-// app.use((req, res, next) => {
-//   let validIps = [
-//     '::ffff:127.0.0.1',
-//     process.env.IP_LOCAL,
-//     process.env.IP_HOME,
-//   ];
-
-//   if (validIps.includes(req.socket.remoteAddress)) {
-//     next();
-//   } else {
-//     // TODO: real log
-//     console.log('IP not on whitelist: ' + req.socket.remoteAddress);
-//     const err = new Error('Not Allowed');
-//     next(err);
-//   }
-// });
-
-// app.use((err, req, res, next) => {
-//   // console.error(err.stack);
-//   res.status(403).send('Forbidden');
-// });
-
 router.use('/worker', workerRouter);
 router.use('/posts', postsRouter);
-router.use('/login', loginRouter);
+router.use('/user', userRouter);
 
-router.get('/', (req, res) => {
-  res.send('API - /api/v1');
+app.get('/', (req, res) => {
+  res.status(404).send();
 });
+app.get('*', (req, res) => res.redirect('/'));
 
 app.listen(port, () => {
   console.log(`API running on port ${port}`);
---
+++ api/routes/userRoutes.mjs
@@ -0,0 +1,88 @@
+import express from 'express';
+import db from '../db/conn.mjs';
+import jwt from 'jsonwebtoken';
+export const userRouter = express.Router();
+
+const usersCollection = db.collection('users');
+
+// User login
+userRouter.post('/auth', async (req, res) => {
+  console.log('POST: /user/auth');
+  const { email, password } = req.body;
+  const user = await usersCollection.findOne({ email });
+
+  if (user && user.password === password) {
+    // TODO: bcrypt
+    const userId = user._id;
+    const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+      expiresIn: '30d',
+    });
+
+    res.cookie('jwt', token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV !== 'development',
+      sameSite: 'strict',
+      maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+    });
+
+    res.json({
+      _id: user._id,
+      email: user.email,
+      favorites: user.favorites,
+    });
+  } else {
+    res.status(401).json({ error: 'invalid credentials' });
+  }
+});
+
+// Register new user
+userRouter.post('/register', async (req, res) => {
+  console.log('POST: /user/register');
+
+  const { email, phone, password } = req.body;
+
+  try {
+    // Validate email
+    if (!email || !email.match(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,8}$/)) {
+      throw new Error('bad email');
+    }
+
+    // Validate password
+    if (!password || !password.match(/^[\w\d!\@\#\$\%\^\&\*\?]{8,128}$/)) {
+      throw new Error('bad password');
+    }
+
+    // Validate phone
+    if (!phone) {
+      phone = 0;
+    } else if (phone.toString().length !== 10) {
+      throw new Error('bad phone');
+    }
+
+    // Validate notification settings
+    // TODO
+
+    // Check for existing user
+    const existingUser = await usersCollection.findOne({ email });
+    if (existingUser) throw new Error('user already exists');
+
+    // Add new user to db
+    // TODO
+
+    // Make a cookie
+    // TODO
+
+    res.json({
+      email: email,
+      phone: phone,
+      password: password,
+    });
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
+
+// Logout current user
+userRouter.get('/logout', (req, res) => {
+  // TODO
+});
