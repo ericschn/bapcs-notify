c84f8a84f7851026ec6ededc32fae4bc67dfde34
ericschn <eschneider327@gmail.com>
2024-03-17 21:20:57 -0500

    add user agent header

--- api/routes/worker.js
+++ api/routes/worker.js
@@ -197,7 +197,11 @@ async function getRedditNew(limit = 25, after = null) {
     }
     let result = await axios.get(`${bapcsUrl}${afterStr}&limit=${limit}`, {
       timeout: 6000,
-      headers: { 'accept-encoding': '*' },
+      headers: {
+        'accept-encoding': '*',
+        'User-Agent':
+          'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246',
+      },
     });
     return result.data.data.children;
   } catch (error) {
